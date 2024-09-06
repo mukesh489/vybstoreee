b2d119644505c05a1eba13186c6bc89a68557f88
mukesh489 <my6888147@gmail.com>
2024-09-06 06:16:58 +0530

    Updated Faq component

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,22 +1,22 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import Accordion from "./Accordion";
 import "../styles/mobile.css";
+
 const Faq = () => {
   return (
-    <section className="container w-full h-full">
-      <div className="flex flex-col w-full h-full">
-        <div>
+    <section className="container sm:w-full md:h-full lg:flex lg:flex-col">
+      <div className="flex flex-col sm:w-full md:h-full">
+        <div className="sm:text-lg md:text-xl">
           <p>FREQUENTLY ASKED QUESTIONS</p>
         </div>
-        <div>
+        <div className="sm:text-sm md:text-base">
           <p>
-            Quick answers to questions you may have. Can&apos;t find what
-            you&apos;re looking for?
+            Quick answers to questions you may have. Can't find what
+            you're looking for?
           </p>
           <p>Check out our full documentation</p>
         </div>
-        <div>
+        <div className="sm:w-full md:flex md:flex-col">
           <Accordion />
         </div>
       </div>
