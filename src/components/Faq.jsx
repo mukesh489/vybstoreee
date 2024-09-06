a8199ee98016dd15531955368a6cca6cfb9c1576
mukesh489 <my6888147@gmail.com>
2024-09-06 06:21:34 +0530

    Updated Faq component

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -4,19 +4,19 @@ import "../styles/mobile.css";
 
 const Faq = () => {
   return (
-    <section className="container sm:w-full md:h-full lg:flex lg:flex-col">
-      <div className="flex flex-col sm:w-full md:h-full">
-        <div className="sm:text-lg md:text-xl">
+    <section className="container sm:w-full md:h-full lg:flex lg:flex-col justify-center items-center p-4">
+      <div className="flex flex-col sm:w-full md:h-full justify-center items-center">
+        <div className="sm:text-lg md:text-xl text-center mb-4">
           <p>FREQUENTLY ASKED QUESTIONS</p>
         </div>
-        <div className="sm:text-sm md:text-base">
+        <div className="sm:text-sm md:text-base text-center mb-4">
           <p>
             Quick answers to questions you may have. Can't find what
             you're looking for?
           </p>
           <p>Check out our full documentation</p>
         </div>
-        <div className="sm:w-full md:flex md:flex-col">
+        <div className="sm:w-full md:flex md:flex-col justify-center items-center">
           <Accordion />
         </div>
       </div>
